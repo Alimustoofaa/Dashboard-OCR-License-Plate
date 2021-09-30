df77eed576d0bcb87e0094e64002862c9fcfb6a2
Alimustoofaa <softhouse.ali@gmail.com>
2021-10-01 02:59:06 +0800

    fix bug

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=8081, debug=True, workers=3)
+    uvicorn.run("main:app",host='0.0.0.0', port=8081, workers=2)
